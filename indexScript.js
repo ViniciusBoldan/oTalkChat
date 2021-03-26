8d15bb2a31caf2596da347a453473b12ca5c3b23
ViniciusBoldan <74440733+ViniciusBoldan@users.noreply.github.com>
2021-03-26 14:46:28 -0400

    Add files via upload

---
+++ indexScript.js
@@ -0,0 +1,6 @@
+function saveLogin(){
+
+    localStorage.setItem('userName', document.getElementById('userName').value)
+    localStorage.setItem('userEmail', document.getElementById('userEmail').value)
+
+}
