7505a84fcc682a03c3026581494bfd2dd1fbd14c
ViniciusBoldan <74440733+ViniciusBoldan@users.noreply.github.com>
2021-03-26 14:59:36 -0400

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -1,3 +1,7 @@
+if(localStorage.mainPageAddress){
+            location.href = localStorage.getItem('mainPageAddress')
+}
+
 function saveLogin(){
 
     localStorage.setItem('userName', document.getElementById('userName').value)
